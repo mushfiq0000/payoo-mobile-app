4ffcb35e768cfae2a7a8afe1620965eebddef19e
mushfiq0000 <kazi.mushfiq.ehsan@gmail.com>
2025-08-25 04:21:26 +0600

    finish the add-money, acount-number and pin number feature using html and js

--- js/home.js
+++ js/home.js
@@ -1,16 +1,86 @@
+const validPin = 1234
+
+// Add Money Feature------------|
+
 document.getElementById('add-money-btn').addEventListener('click', function (e) {
     e.preventDefault()
-    console.log('add momey')
 
     const bank = document.getElementById('bank').value
+
     const acountNumber = document.getElementById('acount-number').value
+
     const amount = parseInt(document.getElementById('add-amount').value)
-    const pin = document.getElementById('add-pin').value
     
+    const pin =parseInt(document.getElementById('add-pin').value)
+
     const availableBalance = parseInt(document.getElementById('available-balance').innerText)
+
+    if (acountNumber.length < 11) {
+        alert('Please Provide Valid Number')
+        return;
+    }
+    if (acountNumber.length >= 12) {
+        alert('Please Provide Valid Number')
+        return;
+    }
+    if (pin != validPin) {
+        alert('please provide valid pin number')
+        return;
+    }
 
     const totalNewvAilableBalance = amount + availableBalance
 
     document.getElementById('available-balance')
-    .innerText = totalNewvAilableBalance    
+    .innerText = totalNewvAilableBalance
+})
+
+
+
+// Cash Out Feature
+
+
+document.getElementById('Withdraw-btn').addEventListener('click', function (e) {
+    e.preventDefault()
+
+    const agentNumber = document.getElementById('agent-number').value
+
+    const amount = parseInt(document.getElementById('withdraw-amount').value)
+
+    const pin =parseInt(document.getElementById('pin-num').value)
+
+    const availableBalance = parseInt(document.getElementById('available-balance').innerText)
+
+    if (agentNumber.length < 11) {
+        alert('Please Provide Valid Number')
+        return;
+    }
+    if (agentNumber.length >= 12) {
+        alert('Please Provide Valid Number')
+        return;
+    }
+    if (pin != validPin) {
+        alert('please provide valid pin number')
+        return;
+    }
+
+    const totalNewvAilableBalance = availableBalance - amount
+ 
+    document.getElementById('available-balance')
+    .innerText = totalNewvAilableBalance
+    
+})
+
+
+
+
+// toggling feature--------------|
+
+document.getElementById('add-button').addEventListener('click', function () {
+    document.getElementById('cash-out-parent').style.display = 'none'
+    document.getElementById('add-money-parent').style.display = 'block'
+})
+
+document.getElementById('cash-out-button').addEventListener('click', function () {
+    document.getElementById('cash-out-parent').style.display ='block'
+    document.getElementById('add-money-parent').style.display = 'none'
 })
